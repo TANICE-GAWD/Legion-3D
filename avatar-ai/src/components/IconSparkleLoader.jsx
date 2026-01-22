307f145cf34ac94a7c06b00e3476505f49d1a259
TANICE-GAWD <2006princesharma@gmail.com>
2026-01-23 00:59:42 +0530

    fixing the voice

---
+++ avatar-ai/src/components/IconSparkleLoader.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const IconSparkleLoader = ({ className = "" }) => {
+  return (
+    <svg
+      className={className}
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M12 2L13.09 8.26L20 9L13.09 9.74L12 16L10.91 9.74L4 9L10.91 8.26L12 2Z"
+        fill="currentColor"
+      />
+      <path
+        d="M19 3L19.5 5.5L22 6L19.5 6.5L19 9L18.5 6.5L16 6L18.5 5.5L19 3Z"
+        fill="currentColor"
+      />
+      <path
+        d="M19 15L19.5 17.5L22 18L19.5 18.5L19 21L18.5 18.5L16 18L18.5 17.5L19 15Z"
+        fill="currentColor"
+      />
+    </svg>
+  );
+};
+
+export default IconSparkleLoader;
