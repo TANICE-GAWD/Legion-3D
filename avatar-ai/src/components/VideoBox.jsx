5aef6ae662aca84ede0d6b61a9c96a4291692332
TANICE-GAWD <2006princesharma@gmail.com>
2026-01-23 00:59:26 +0530

    asimli

---
+++ avatar-ai/src/components/VideoBox.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const VideoBox = ({ video, audio }) => {
+  return (
+    <div className="w-full h-full relative">
+      <video
+        ref={video}
+        autoPlay
+        playsInline
+        className="w-full h-full object-cover"
+      />
+      <audio ref={audio} autoPlay />
+    </div>
+  );
+};
+
+export default VideoBox;
